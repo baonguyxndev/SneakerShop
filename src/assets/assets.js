466f4937cdaa0802a5a8eb4d2bb95da817cdc5b8
yonB280405 <giabao2005bbb@gmail.com>
2024-08-21 18:27:02 +0700

    add: file assets, navbar

---
+++ src/assets/assets.js
@@ -0,0 +1,361 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import logo_footer from './logo_footer.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './nike.png'
+import menu_2 from './adidas.png'
+import menu_3 from './puma.png'
+import menu_4 from './ananas.png'
+import menu_5 from './new-balance.png'
+import menu_6 from './converse.jpg'
+import menu_7 from './vans.png'
+import menu_8 from './bitis.png'
+
+//NIKE SNEAKER
+import nike_sneaker1 from './air-force-1-low.png'
+import nike_sneaker2 from './air-force-1-mid.png'
+import nike_sneaker3 from './air-force-1-high.png'
+import nike_sneaker4 from './lunar-force-1.png'
+import nike_sneaker5 from './air-max-dn-shoes.png'
+import nike_sneaker6 from './air-max-plus.png'
+import nike_sneaker7 from './air-vapormax.png'
+import nike_sneaker8 from './air-max-270.png'
+import nike_sneaker9 from './air-max-1-essential.png'
+import nike_sneaker10 from './air-max-alpha-trainer-5.png'
+import nike_sneaker11 from './air-vapormax-plus.png'
+import nike_sneaker12 from './air-max-90.png'
+import nike_sneaker13 from './air-max-excee.png'
+import nike_sneaker14 from './air-max-ishod.png'
+
+
+//ADIDAS SNEAKER
+import adidas_sneaker1 from './samba-og-shoes.avif'
+import adidas_sneaker2 from './sambae-shoes.avif'
+import adidas_sneaker3 from './samba-og-shoes-kids.avif'
+import adidas_sneaker4 from './samba-low-trainers.avif'
+import adidas_sneaker5 from './pharrell-williams-humanrace-samba-shoes.avif'
+import adidas_sneaker6 from './samba-decon-shoes.avif'
+import adidas_sneaker7 from './sambae-x-kseniaschnaider-shoes.avif'
+import adidas_sneaker8 from './samba-og-w.avif'
+import adidas_sneaker9 from './handball-spezial-shoes.avif'
+import adidas_sneaker10 from './gazelle-indoor-shoes.avif'
+import adidas_sneaker11 from './gazelle-up-shoes.avif'
+import adidas_sneaker12 from './pride-gazelle-bold-pabllo-vittar-shoes.avif'
+import adidas_sneaker13 from './gazelle-bold-shoes.avif'
+import adidas_sneaker14 from './gazelle-shoes-kids.avif'
+import adidas_sneaker15 from './gazelle-85-shoes.avif'
+import adidas_sneaker16 from './sl-72-rs-shoes.avif'
+
+
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    logo_footer,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "NIKE",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "ADIDAS",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "PUMA",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "ANANAS",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "NEW BALANCE",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "CONVERSE",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "VANS",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "BITI'S",
+        menu_image: menu_8
+    }]
+
+export const food_list = [
+
+    //NIKE
+    {
+        _id: "1",
+        name: "AIR FORCE 1 LOW",
+        image: nike_sneaker1,
+        price: 12,
+        description: "NIKE SNEAKER",
+        category: "NIKE"
+    },
+    {
+        _id: "2",
+        name: "AIR FORCE 1 MID",
+        image: nike_sneaker2,
+        price: 18,
+        description: "NIKE SNEAKER",
+        category: "NIKE"
+    }, {
+        _id: "3",
+        name: "AIR FORCE 1 HIGH",
+        image: nike_sneaker3,
+        price: 16,
+        description: "NIKE SNEAKER",
+        category: "NIKE"
+    }, {
+        _id: "4",
+        name: "LUNAR FORCE 1",
+        image: nike_sneaker4,
+        price: 24,
+        description: "NIKE SNEAKER",
+        category: "NIKE"
+    }, {
+        _id: "5",
+        name: "AIR MAX DN",
+        image: nike_sneaker5,
+        price: 14,
+        description: "NIKE SNEAKER",
+        category: "NIKE"
+    }, {
+        _id: "6",
+        name: "AIR MAX PLUS",
+        image: nike_sneaker6,
+        price: 12,
+        description: "NIKE SNEAKER",
+        category: "NIKE"
+    }, {
+        _id: "7",
+        name: "AIR VAPORMAX",
+        image: nike_sneaker7,
+        price: 20,
+        description: "NIKE SNEAKER",
+        category: "NIKE"
+    }, {
+        _id: "8",
+        name: "AIR MAX 270",
+        image: nike_sneaker8,
+        price: 15,
+        description: "NIKE SNEAKER",
+        category: "NIKE"
+    }, {
+        _id: "9",
+        name: "AIR MAX 1 ESSENTIAL",
+        image: nike_sneaker9,
+        price: 14,
+        description: "NIKE SNEAKER",
+        category: "NIKE"
+    }, {
+        _id: "10",
+        name: "AIR MAX ALPHA TRAINER",
+        image: nike_sneaker10,
+        price: 22,
+        description: "NIKE SNEAKER",
+        category: "NIKE"
+    }, {
+        _id: "11",
+        name: "AIR VAPORMAX PLUS",
+        image: nike_sneaker11,
+        price: 10,
+        description: "NIKE SNEAKER",
+        category: "NIKE"
+    }, {
+        _id: "12",
+        name: "AIR MAX 90",
+        image: nike_sneaker12,
+        price: 12,
+        description: "NIKE SNEAKER",
+        category: "NIKE"
+    },
+    {
+        _id: "13",
+        name: "AIR MAX EXCEE",
+        image: nike_sneaker13,
+        price: 12,
+        description: "NIKE SNEAKER",
+        category: "NIKE"
+    },
+    {
+        _id: "14",
+        name: "AIR MAC ISHOD",
+        image: nike_sneaker14,
+        price: 18,
+        description: "NIKE SNEAKER",
+        category: "NIKE"
+    },
+
+
+    //ADIDAS
+    {
+        _id: "15",
+        name: "AIR MAC ISHOD",
+        image: adidas_sneaker1,
+        price: 18,
+        description: "ADIDAS SNEAKER",
+        category: "ADIDAS"
+    },
+    {
+        _id: "16",
+        name: "AIR MAC ISHOD",
+        image: adidas_sneaker2,
+        price: 18,
+        description: "ADIDAS SNEAKER",
+        category: "ADIDAS"
+    },
+    {
+        _id: "17",
+        name: "AIR MAC ISHOD",
+        image: adidas_sneaker3,
+        price: 18,
+        description: "ADIDAS SNEAKER",
+        category: "ADIDAS"
+    },
+    {
+        _id: "18",
+        name: "AIR MAC ISHOD",
+        image: adidas_sneaker4,
+        price: 18,
+        description: "ADIDAS SNEAKER",
+        category: "ADIDAS"
+    },
+    {
+        _id: "19",
+        name: "AIR MAC ISHOD",
+        image: adidas_sneaker5,
+        price: 18,
+        description: "ADIDAS SNEAKER",
+        category: "ADIDAS"
+    },
+    {
+        _id: "20",
+        name: "AIR MAC ISHOD",
+        image: adidas_sneaker6,
+        price: 18,
+        description: "ADIDAS SNEAKER",
+        category: "ADIDAS"
+    },
+    {
+        _id: "21",
+        name: "AIR MAC ISHOD",
+        image: adidas_sneaker7,
+        price: 18,
+        description: "ADIDAS SNEAKER",
+        category: "ADIDAS"
+    },
+    {
+        _id: "22",
+        name: "AIR MAC ISHOD",
+        image: adidas_sneaker8,
+        price: 18,
+        description: "ADIDAS SNEAKER",
+        category: "ADIDAS"
+    },
+    {
+        _id: "23",
+        name: "AIR MAC ISHOD",
+        image: adidas_sneaker9,
+        price: 18,
+        description: "ADIDAS SNEAKER",
+        category: "ADIDAS"
+    },
+    {
+        _id: "24",
+        name: "AIR MAC ISHOD",
+        image: adidas_sneaker10,
+        price: 18,
+        description: "ADIDAS SNEAKER",
+        category: "ADIDAS"
+    },
+    {
+        _id: "25",
+        name: "AIR MAC ISHOD",
+        image: adidas_sneaker11,
+        price: 18,
+        description: "ADIDAS SNEAKER",
+        category: "ADIDAS"
+    },
+    {
+        _id: "26",
+        name: "AIR MAC ISHOD",
+        image: adidas_sneaker12,
+        price: 18,
+        description: "ADIDAS SNEAKER",
+        category: "ADIDAS"
+    },
+    {
+        _id: "27",
+        name: "AIR MAC ISHOD",
+        image: adidas_sneaker13,
+        price: 18,
+        description: "ADIDAS SNEAKER",
+        category: "ADIDAS"
+    },
+    {
+        _id: "28",
+        name: "AIR MAC ISHOD",
+        image: adidas_sneaker14,
+        price: 18,
+        description: "ADIDAS SNEAKER",
+        category: "ADIDAS"
+    },
+    {
+        _id: "29",
+        name: "AIR MAC ISHOD",
+        image: adidas_sneaker15,
+        price: 18,
+        description: "ADIDAS SNEAKER",
+        category: "ADIDAS"
+    },
+    {
+        _id: "30",
+        name: "AIR MAC ISHOD",
+        image: adidas_sneaker16,
+        price: 18,
+        description: "ADIDAS SNEAKER",
+        category: "ADIDAS"
+    },
+]   

d6c900db64a15a84d8173793dc147b261ba015e3
yonB280405 <giabao2005bbb@gmail.com>
2024-08-23 14:57:45 +0700

    fix: header-banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './pages/Cart/Cart'
+import Home from './pages/Home/Home'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
-const App = () => {
 
+const App = () => { 
   return (
     <div className='app'>
-      <Navbar/>
+    <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order' element={<PlaceOrder/>}/>
+      </Routes>
     </div>
-
   ) 
 }
 
